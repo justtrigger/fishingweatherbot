546a9c96d3bf27b2d29ac8d34e27f92d7a9c05e7
Тебекин Дмитрий <trigger3@yandex.ru>
2023-09-01 16:30:53 +0300

    v4 update install.sh etc

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-mkdir /usr/fishingweatherbot
-cp main.py /usr/fishingweatherbot
+apt update $$ apt install -y python3 python3-pip
+pip install -r requirements.txt
+mkdir /usr/share/fishingweatherbot
+cp main.py /usr/share/fishingweatherbot
 cp fishingweatherbot.service /etc/systemd/system/
 systemctl enable --now fishingweatherbot
