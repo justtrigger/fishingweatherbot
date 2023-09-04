44d0489e520378f23be0df568ce71c2d2998db2c
Дмитрий Тебекин <trigger3@yandex.ru>
2023-09-04 20:52:46 +0300

    v7 remaked menu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import datetime
+import datetime as dt
 import requests
 import logging
 from dotenv import load_dotenv
@@ -13,7 +13,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 OPENWEATHERMAP_TOKEN = os.getenv('OPENWEATHERMAP_TOKEN')
 OPENWEATHERMAP_ENDPOINT = "api.openweathermap.org"
-LOG_FILE=os.getenv('LOG_FILE')
+LOG_FILE = os.getenv('LOG_FILE')
 
 
 bot = Bot(token=BOT_TOKEN)
@@ -26,40 +26,44 @@ async def start_command(message: types.Message):
     await go_to_menu(message)
 
 
-async def get_weather(city, date, message):
+async def get_weather(place, date, message):
     try:
         go_to_menu(message) 
-        logging.info(f"{message.from_id} requesting {city}")
-        response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&units=metric&appid={OPENWEATHERMAP_TOKEN}")
+        logging.info(f"{message.from_id} requesting {place}")
+        response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?{place}&lang=ru&units=metric&appid={OPENWEATHERMAP_TOKEN}")
         data = response.json()
-        city = data["name"]
+        place = data["name"]
         cur_temp = data["main"]["temp"]
         humidity = data["main"]["humidity"]
         pressure = data["main"]["pressure"]
         wind = data["wind"]["speed"]
 
-        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        sunrise_timestamp = dt.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = dt.datetime.fromtimestamp(data["sys"]["sunset"])
 
         # продолжительность дня
-        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) -       datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        length_of_the_day = (dt.datetime.fromtimestamp(data["sys"]["sunset"]) -
+                             dt.datetime.fromtimestamp(data["sys"]["sunrise"]))
         await message.reply(f"{date}\n"
-                            f"Погода в городе: {city}\nТемпература: {cur_temp}°C\n"
-                            f"Влажность: {humidity}%\nДавление: {round(pressure/1.333)} мм.рт.ст\nВетер: {wind} м/с \n"
+                            f"Погода в городе: {place}\nТемпература: {cur_temp}°C\n"
+                            f"Влажность: {humidity}%\nДавление: 
+                              {round(pressure/1.333)} мм.рт.ст\nВетер: {wind} м/с\n"
                             f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
                             f"Ни хвоста, ни чешуи"
                             )
     except Exception as error:
         logging.exception(error)
-        
+     
 
 @dp.message_handler()
 async def go_to_menu(message: types.Message):
-    global city
+    global place
     global date
-    cities = ["Курск", "Курчатов", "Суджа", "Железногорск"]
+    cities = ["Курск", "Железногорск", "Курчатов", "Льгов", "Щигры",
+              "Рыльск", "Обоянь", "Дмитриев", "Суджа", "Фатеж"]
     ponds = ["Стадион"]
-    dates = ["Погода сейчас", "Прогноз на сегодня", "Прогноз на завтра", "Прогноз на выходные"]
+    dates = ["Погода сейчас", "Прогноз на сегодня",
+             "Прогноз на завтра", "Прогноз на выходные"]
     main_menu = [
         [
             types.KeyboardButton(text="Выбор города"),
@@ -67,42 +71,26 @@ async def go_to_menu(message: types.Message):
         ],
         [
             types.KeyboardButton(text="Выбор дня"),
+            types.KeyboardButton(text="Выбор API")
+        ],
+        [
             types.KeyboardButton(text="Главное меню")
         ]
     ]
     city_menu = [
-       [
-           types.KeyboardButton(text="Курск"),
-           types.KeyboardButton(text="Курчатов"),
-       ],
-       [
-           types.KeyboardButton(text="Суджа"),
-           types.KeyboardButton(text="Железногорск")
-       ],
-       [
-           types.KeyboardButton(text="Главное меню")
-       ]
-   ]
+        [types.KeyboardButton(text=city) for city in
+         cities + ["Главное меню"]][i:i + 2] for i in
+        range(0, len(cities) + 1, 2)
+    ]
     pond_menu = [
-        [
-            types.KeyboardButton(text="Стадион"),
-        ],
-        [
-            types.KeyboardButton(text="Главное меню")
-        ]
+        [types.KeyboardButton(text=pond) for pond in
+         ponds + ["Главное меню"]][i:i + 2] for i in
+        range(0, len(ponds) + 1, 2)
     ]
     date_menu = [
-        [
-            types.KeyboardButton(text="Погода сейчас"),
-            types.KeyboardButton(text="Прогноз на сегодня"),
-        ],
-        [
-            types.KeyboardButton(text="Прогноз на завтра"),
-            types.KeyboardButton(text="Прогноз на выходные"),
-        ],
-        [
-            types.KeyboardButton(text="Главное меню")
-        ]
+        [types.KeyboardButton(text=date) for date in
+         dates + ["Главное меню"]][i:i + 2] for i in
+        range(0, len(dates) + 1, 2)
     ]
     menu_levels = {
         "Главное меню": main_menu,
@@ -115,13 +103,13 @@ async def go_to_menu(message: types.Message):
         await message.reply(f"{message.text}", reply_markup=keyboard)
     except KeyError:
         if message.text in cities:
-                city = message.text
-                date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+                place = f"q={message.text}"
+                date = dt.datetime.now().strftime('%Y-%m-%d %H:%M')
         if message.text in ponds:
                 await message.reply(f"функционала пока нет")
         if message.text in dates:
                 await message.reply(f"функционала пока нет")
-        await get_weather(city, date, message)
+        await get_weather(place, date, message)
 
 
 if __name__ == "__main__":
@@ -131,7 +119,7 @@ if __name__ == "__main__":
             filemode="w",
             format="%(asctime)s %(levelname)s %(message)s"
             )
-    city = "Курск"
-    date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+    place = "Курск"
+    date = dt.datetime.now().strftime('%Y-%m-%d %H:%M')
     logging.info(f"Start polling.")
     executor.start_polling(dp)
