ce29ec4acdff2ff94f8fc9be388cfd164f56ef6a
Тебекин Дмитрий <trigger3@yandex.ru>
2023-09-04 08:54:21 +0300

    v5 add logging

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import requests
 import math
+import logging
 from dotenv import load_dotenv
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
@@ -12,18 +13,21 @@ load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 OPENWEATHERMAP_TOKEN = os.getenv('OPENWEATHERMAP_TOKEN')
 OPENWEATHERMAP_ENDPOINT = "api.openweathermap.org"
+LOG_FILE=os.getenv('LOG_FILE')
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
-	await message.reply("Привет! Напиши мне название города и я пришлю сводку погоды")
+    logging.info(f"{message.from_id} подключился к боту")
+    await message.reply("Введите название города для получения текущей сводки")
 
 @dp.message_handler()
 async def get_weather(message: types.Message):
     try:
         city = message.text
+        logging.info(f"{message.from_id} заправшивает {city}")
         response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&units=metric&appid={OPENWEATHERMAP_TOKEN}")
         data = response.json()
         city = data["name"]
@@ -41,14 +45,19 @@ async def get_weather(message: types.Message):
                             f"Погода в городе: {city}\nТемпература: {cur_temp}°C\n"
                             f"Влажность: {humidity}%\nДавление: {math.ceil(pressure/1.333)} мм.рт.ст\nВетер: {wind} м/с \n"
                             f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
-                            f"Хорошего дня!"
+                            f"Ни хвоста, ни чешуи"
                             )
     except Exception as error:
-        print(error)
+        logging.error(f"{error}")
         
 
 
 if __name__ == "__main__":
-	# С помощью метода executor.start_polling опрашиваем
-    # Dispatcher: ожидаем команду /start
-	executor.start_polling(dp)
+    logging.basicConfig(
+            level=logging.INFO,
+            filename=LOG_FILE,
+            filemode="w",
+            format="%(asctime)s %(levelname)s %(message)s"
+            )
+    logging.info(f"Start polling.")
+    executor.start_polling(dp)
