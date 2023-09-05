ba1f452e7e4044a2d058ec3ae92c0c1442a8331b
Тебекин Дмитрий <trigger3@yandex.ru>
2023-09-05 16:51:23 +0300

    v8 add ponds and dates

--- main.py
+++ main.py
@@ -23,30 +23,35 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
     logging.info(f"{message.from_id} connected")
-    await go_to_menu(message)
+    go_to_menu(message)
 
 
-async def get_weather(place, date, message):
+async def get_weather(place, date, forecast_type, message):
     try:
-        go_to_menu(message) 
+        go_to_menu(message, place, forecast_type) 
         logging.info(f"{message.from_id} requesting {place}")
-        response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?{place}&lang=ru&units=metric&appid={OPENWEATHERMAP_TOKEN}")
+        logging.info(f"http://api.openweathermap.org/data/2.5/{forecast_type}?{place}&lang=ru&units=metric&appid={OPENWEATHERMAP_TOKEN}")
+        response = requests.get(f"http://api.openweathermap.org/data/2.5/{forecast_type}?{place}&lang=ru&units=metric&appid={OPENWEATHERMAP_TOKEN}")
         data = response.json()
+        logging.info(data)
         place = data["name"]
         cur_temp = data["main"]["temp"]
         humidity = data["main"]["humidity"]
         pressure = data["main"]["pressure"]
-        wind = data["wind"]["speed"]
-
-        sunrise_timestamp = dt.datetime.fromtimestamp(data["sys"]["sunrise"])
-        sunset_timestamp = dt.datetime.fromtimestamp(data["sys"]["sunset"])
+        wind_speed = data["wind"]["speed"]
+        wind_gust = round(wind_speed + data["wind"]["gust"], 2)
+            winddirections = ("северный", "северо-восточный", "восточный", "юго-восточный", "южный", "юго-западный", "западный", "северо-западный")
+            wind_deg = int((data["wind"]["deg"] + 22.5) // 45 % 8)
+            wind_dir = winddirections[wind_deg]
+        sunrise_timestamp = dt.datetime.fromtimestamp(data["sys"]["sunrise"]).strftime('%H:%M:%S')
+        sunset_timestamp = dt.datetime.fromtimestamp(data["sys"]["sunset"]).strftime('%H:%M:%S')
 
         # продолжительность дня
         length_of_the_day = (dt.datetime.fromtimestamp(data["sys"]["sunset"]) -
                              dt.datetime.fromtimestamp(data["sys"]["sunrise"]))
-        await message.reply(f"{date}\n"
-                            f"Погода в городе: {place}\nТемпература: {cur_temp}°C\n"
-                            f"Влажность: {humidity}%\nДавление: {round(pressure/1.333)} мм.рт.ст\nВетер: {wind} м/с\n"
+        await message.reply(f"{saved_user_data[message.from_id]['forecast_type']}: {saved_user_data[message.from_id]['place']}\nТемпература: {cur_temp}°C\n"
+                            f"Влажность: {humidity}%\nДавление: {round(pressure/1.333)} мм.рт.ст\n"
+                            f"Ветер: {wind_dir},\n{wind_speed} м/с, порывы до {wind_gust} м/с\n"
                             f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
                             f"Ни хвоста, ни чешуи"
                             )
@@ -55,14 +60,30 @@ async def get_weather(place, date, message):
      
 
 @dp.message_handler()
-async def go_to_menu(message: types.Message):
-    global place
-    global date
+async def go_to_menu(message: types.Message, place="Курск", forecast_type="weather", api="openweather"):
+    global saved_user_data
+    if message.from_id not in saved_user_data:
+         saved_user_data[message.from_id] = {
+              "place": "Курск",
+              "forecast_type": "Погода сейчас",
+              "api" : "openweather"
+         }
+    else:
+        if saved_user_data[message.from_id]["place"] is None:
+            saved_user_data[message.from_id]["place"] = "Курск"
+        if saved_user_data[message.from_id]["forecast_type"] is None:
+            saved_user_data[message.from_id]["forecast_type"] = "weather"
+        if saved_user_data[message.from_id]["api"] is None:
+            saved_user_data[message.from_id]["api"] = "openweather"
+    date = dt.datetime.now().strftime('%Y-%m-%d %H:%M')
     cities = ["Курск", "Железногорск", "Курчатов", "Льгов", "Щигры",
-              "Рыльск", "Обоянь", "Дмитриев", "Суджа", "Фатеж"]
-    ponds = ["Стадион"]
-    dates = ["Погода сейчас", "Прогноз на сегодня",
-             "Прогноз на завтра", "Прогноз на выходные"]
+              "Рыльск", "Обоянь", "Суджа", "Фатеж"]
+    ponds = {"Стадион": "lat=51.831599&lon=36.010743",
+             "Пойменово": "lat=51.961621&lon=36.244221"}
+    dates = {"Погода сейчас": "weather",
+             "Прогноз на сегодня": "forecast",
+             "Прогноз на завтра" : "forecast",
+             "Прогноз на выходные": "forecast"}
     main_menu = [
         [
             types.KeyboardButton(text="Выбор города"),
@@ -83,12 +104,12 @@ async def go_to_menu(message: types.Message):
     ]
     pond_menu = [
         [types.KeyboardButton(text=pond) for pond in
-         ponds + ["Главное меню"]][i:i + 2] for i in
+         list(ponds.keys()) + ["Главное меню"]][i:i + 2] for i in
         range(0, len(ponds) + 1, 2)
     ]
     date_menu = [
         [types.KeyboardButton(text=date) for date in
-         dates + ["Главное меню"]][i:i + 2] for i in
+         list(dates.keys()) + ["Главное меню"]][i:i + 2] for i in
         range(0, len(dates) + 1, 2)
     ]
     menu_levels = {
@@ -103,12 +124,15 @@ async def go_to_menu(message: types.Message):
     except KeyError:
         if message.text in cities:
                 place = f"q={message.text}"
-                date = dt.datetime.now().strftime('%Y-%m-%d %H:%M')
+                saved_user_data[message.from_id]["place"] = message.text
         if message.text in ponds:
-                await message.reply(f"функционала пока нет")
+                place = ponds[message.text]
+                saved_user_data[message.from_id]["place"] = message.text
         if message.text in dates:
-                await message.reply(f"функционала пока нет")
-        await get_weather(place, date, message)
+                place = f"q={place}"
+                forecast_type = dates[message.text]
+                saved_user_data[message.from_id]["forecast_type"] = message.text
+        await get_weather(place, date, forecast_type, message)
 
 
 if __name__ == "__main__":
@@ -117,8 +141,8 @@ if __name__ == "__main__":
             filename=LOG_FILE,
             filemode="w",
             format="%(asctime)s %(levelname)s %(message)s"
-            )
-    place = "Курск"
+            ) 
     date = dt.datetime.now().strftime('%Y-%m-%d %H:%M')
+    saved_user_data = dict()
     logging.info(f"Start polling.")
     executor.start_polling(dp)
